0b86e8f49d103ba284a3cc11f439ac13dd805dde
Steven <stevenboada@gmail.com>
2015-07-07 11:38:19 -0700

    initial commit

---
+++ data/boada/may_2012/analysis/parseResults.py
@@ -0,0 +1,29 @@
+import pylab as pyl
+import glob
+from numpy import genfromtxt
+
+files = glob.glob('*_results/*.results')
+
+r = []
+for f in files:
+    print f
+    cluster, field, dither = f.split('/')[1].split('_')
+    data = genfromtxt(f, delimiter='\t', names=True, dtype=None)
+    try:
+        for fiber, z, Q in zip(data['Fiber'], data['Redshift'],
+                data['Quality']):
+            if Q == 0:
+                r.append((field, dither.split('.')[0], fiber, z))
+    except TypeError:
+        fiber = int(data['Fiber'])
+        z = float(data['Redshift'])
+        Q = int(data['Quality'])
+        if Q == 0:
+            r.append((field, dither.split('.')[0], fiber, z))
+
+print len(r), 'objects read'
+print r
+
+print len(r)
+
+
