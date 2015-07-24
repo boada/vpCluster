9631cad819272730be5e9e45f298c798c7a75959
Steven <stevenboada@gmail.com>
2015-07-24 17:25:48 -0500

    tweak

--- data/boada/mosaics/plot_cluster.py
+++ data/boada/mosaics/plot_cluster.py
@@ -1,7 +1,7 @@
 import aplpy
 import pylab as pyl
 
-cluster = 'c203p83+41p00_r_mosaic.fits'
+cluster = 'c203p8+41p0_r_mosaic.fits'
 
 f = pyl.figure(1, figsize=(8,8))
 
@@ -11,5 +11,15 @@ gc.set_theme('publication')
 
 gc.set_tick_labels_format(xformat='hh:mm:ss', yformat='dd:mm:ss')
 #gc.set_tick_labels_size('small')
+
+data = pyl.genfromtxt(cluster.split('_')[0]+'_complete.csv', delimiter=',',
+        names=True)
+
+# filter out the specz's
+x = pyl.isnan(data['Specz'])
+
+# draw the specz's
+gc.show_markers(data['ra'][~x], data['dec'][~x], edgecolor='#e24a33',
+        facecolor='none', marker='D', s=200)
 
 
