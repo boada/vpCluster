6b51d46ad3c416eebe6829d1f82453692491cff4
Steven <stevenboada@gmail.com>
2015-07-29 16:58:22 -0500

    updated columns

--- data/boada/analysis_all/mk_latex_tables.py
+++ data/boada/analysis_all/mk_latex_tables.py
@@ -16,8 +16,9 @@ def dms(dec):
         return 'NaN'
 
 def main(cluster):
-    df = pd.read_csv(cluster+'_redshifts.csv')
-    df_part = df[['tile', 'dither', 'fiber', 'ra', 'dec', 'r', 'redshift']]
+    df = pd.read_csv(cluster+'_members.csv')
+    df_part = df[['tile', 'dither', 'fiber', 'ra', 'dec', 'r', 'redshift',
+    'interloper', 'separation']]
 
     # remove the un-observed galaxies
     x = ~isnan(df_part['redshift'])
@@ -26,10 +27,15 @@ def main(cluster):
     # now we are going to round some of the columns to the right length
     df_part[['r']] = df_part[['r']].apply(round, args=(2,))
     df_part[['redshift']] = df_part[['redshift']].apply(round, args=(4,))
+    df_part[['seperation']] = df_part[['separation']].apply(round, args=(2,))
 
     # now we have to convert the ra/dec to sexigesimal
     df_part[['ra']] = [hms(ra) for ra in df_part[['ra']].values.ravel()]
     df_part[['dec']] = [dms(dec) for dec in df_part[['dec']].values.ravel()]
+
+    # replace the interloper with checkmarks
+    df_part.interloper[df_part.interloper == 'NO'] = r'surd'
+    df_part.interloper[df_part.interloper == 'YES'] = r'...'
 
 
     df_part.to_latex(cluster+'_table.tex', index=False)
