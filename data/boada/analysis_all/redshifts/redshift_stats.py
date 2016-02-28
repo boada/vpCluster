b58d296373ed4ba75d0e6409e332e70abea76086
Steven <stevenboada@gmail.com>
2016-02-28 15:29:44 -0600

    add more axes labels

--- data/boada/analysis_all/redshifts/redshift_stats.py
+++ data/boada/analysis_all/redshifts/redshift_stats.py
@@ -18,18 +18,23 @@ for f in files:
         x = ~pyl.isnan(results.fiber) & pyl.isnan(results.Q)
         q2 = results.r.values[x.values]
 
+# make a figure
+f = pyl.figure(1,figsize=(5,5*(pyl.sqrt(5.)-1.0)/2.0))
+ax = f.add_subplot(111)
+
 bins = pyl.linspace(14,22,15)
-pyl.hist(q2, weights=pyl.zeros_like(q2)+1./q2.size, histtype='step', bins=bins,
+ax.hist(q2, weights=pyl.zeros_like(q2)+1./q2.size, histtype='step', bins=bins,
         lw=2, label='Q=2')
-pyl.hist(q1, weights=pyl.zeros_like(q1)+1./q1.size, histtype='step', bins=bins,
+ax.hist(q1, weights=pyl.zeros_like(q1)+1./q1.size, histtype='step', bins=bins,
         lw=2, label='Q=1')
 q0 = q0[~pyl.isnan(q0)]
-pyl.hist(q0, weights=pyl.zeros_like(q0)+1./q0.size, histtype='step', bins=bins,
+ax.hist(q0, weights=pyl.zeros_like(q0)+1./q0.size, histtype='step', bins=bins,
         lw=2, label='Q=0')
 
-pyl.legend(loc='upper right')
-pyl.gca().invert_xaxis()
+ax.legend(loc='upper right')
+ax.invert_xaxis()
 
-pyl.ylim(0,0.5)
-pyl.xlabel('$m_r$')
-
+ax.set_ylim(0,0.5)
+ax.set_xlabel('$m_r$')
+ax.set_ylabel('Fraction of Total')
+pyl.show()
