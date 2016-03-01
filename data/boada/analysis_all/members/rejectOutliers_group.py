ab45c8136c47683774db4926d16475d2acf17596
Steven <stevenboada@gmail.com>
2016-03-01 17:00:01 -0600

    added shifty rapper and corrected pandas problems
    updated some of the pandas command so it doesn’t complain. Added S.
    Crawford’s version of the shifting gapper method. Basically does just
    as good as my version.

--- data/boada/analysis_all/members/rejectOutliers_group.py
+++ data/boada/analysis_all/members/rejectOutliers_group.py
@@ -4,6 +4,12 @@ from astLib import astCoords as aco
 from astLib import astStats as ast
 from astLib import astCalc as aca
 import sys
+from glob import glob
+
+# buzzard simulation cosmology
+aca.H0 = 70
+aca.OMEGA_M0 = 0.286
+aca.OMEGA_L0 = 0.714
 
 c = 2.99E5 # speed of light in km/s
 
@@ -25,12 +31,12 @@ def findseparationSpatial(data, center):
     '''
 
     # Add a new column to the dataframe
-    data['separation'] = 0.0
+    data.loc[:, 'separation'] = 0.0
     for row in data.iterrows():
         sepDeg = aco.calcAngSepDeg(center[0], center[1], row[1]['ra'],
                 row[1]['dec'])
         sepMpc = sepDeg * aca.da(row[1]['redshift'])/57.2957795131
-        data['separation'][row[0]] = sepMpc
+        data.loc[row[0], 'separation'] = sepMpc
         #data['separation'][row[0]] = sepDeg * 3600
 
     return data
@@ -43,9 +49,11 @@ def findLOSV(data, avgz):
     c = 2.99E5 # speed of light in km/s
 
     # Add a new column to the dataframe
-    data['LOSV'] = 0.0
+    data.loc[:, 'LOSV'] = 0.0
+    data.loc[:, 'LOSV_err'] = 0.0
     for row in data.iterrows():
-        data['LOSV'][row[0]] = c *(row[1]['redshift'] - avgz)/(1 + avgz)
+        data.loc[row[0], 'LOSV'] = c *(row[1]['redshift'] - avgz)/(1 + avgz)
+        data.loc[row[0], 'LOSV_err'] = c/(1 + avgz) * row[1]['redshift_err'] * 2
 
     return data
 
@@ -70,7 +78,7 @@ def rejectInterlopers(data):
     data = data[data.interloper == 'NO']
 
     # make some copies so we can sort them around
-    sepSorted = data.sort('separation', ascending=True)
+    sepSorted = data.sort_values(by='separation', ascending=True)
     # How many parts to break into
     parts = len(data)//10
     splitData = split_list(sepSorted, parts)
@@ -79,7 +87,7 @@ def rejectInterlopers(data):
     interlopers = []
     for part in splitData:
         # sort by LOSV
-        LOSVsorted = part.sort('LOSV', ascending=True)
+        LOSVsorted = part.sort_values(by='LOSV', ascending=True)
         rejected = True
         while rejected:
             # Find the difference between all of the neighboring elements
@@ -109,11 +117,72 @@ def rejectInterlopers(data):
                 rejected = False
     print 'interlopers',interlopers
 
-    data['interloper'][interlopers] = 'YES'
+    data.loc[interlopers,'interloper'] = 'YES'
 
     #return data.drop(interlopers)
     #return data_orig.update(data)
     data_orig.update(data)
+
+def shifty_gapper(r, z, zc, vlimit=10000, ngap=30, glimit=1000):
+    '''Determine cluster membersip according to the shifty
+      gapper technique
+      The shifty gapper technique includes the following steps:
+      1.  Remove outliers outside of +- vlimit
+      2.  Locate the Ngap targets with radius close to r_i
+      3.  Within this sample of N targets, identify sources with
+          |v_pec| < |v_pec_i|
+      4.  Meaure the largest gap within this subset of targets
+      5.  If v_gap is larger than glimit, reject the source
+      Parameters
+      -----------
+      vlimit: float
+         Initial limit in velocity offset from the cluster center
+      ngap: int
+         Number of sources to use in in estimating the gap
+      glimit:  float
+         Maximum gap size for rejecting objects
+
+      Parameters
+      -----------
+      incluster: ndarray
+          Returns a boolean array where objects in the cluster have
+          a value of True
+    '''
+
+    def z2v(z, zc):
+        '''Convert the redshift to km/s relative to the cluster center '''
+        return 2.99792458e5*(z-zc)/(1+zc)
+
+    #convert to the velocity scale
+    v = z2v(z,zc)
+
+    #limit the source to only sources within the vlimit
+    vmask = abs(v) < vlimit
+
+    nobj=len(r)
+    incluster=np.zeros(nobj, dtype=bool)
+
+    if nobj<ngap:
+        raise Exception('Number of sources is less thant number of gap sources')
+
+    for i in range(nobj):
+        if abs(v[i])<vlimit:
+            #find the ngap closest sources
+            r_j=abs(r[vmask]-r[i]).argsort()
+            vg=v[vmask][r_j[0:ngap]]
+
+            #find the sources with |v_pec| < |v_pec_i|
+            mask=abs(vg)<=abs(v[i])
+            if mask.sum()>1:
+                vg=vg[mask]
+                #now sort these sources and find the biggest gap
+                vg.sort()
+                if np.diff(vg).max()<glimit: incluster[i]=True
+            else:
+                incluster[i]=True
+
+    return incluster
+
 
 def rejectInterlopers_group(data, avgz, sigmav=500):
 
@@ -140,77 +209,91 @@ if __name__ == "__main__":
 
     '''
 
-    catalog = './../redshifts/'+sys.argv[1]+'_redshifts.csv'
-    # get the center
-    with open('./../centers/'+sys.argv[1]+'_center.list', 'r') as f:
-        line = f.read()
-    line = line.split(' ')
-    center = float(line[0]), float(line[1])
+    if len(sys.argv) == 1:
+        clusters = glob('../redshifts/*_redshifts.csv')
+        # strip off the extra wording
+        clusters = [c.rstrip('_redshifts.csv') for c in clusters]
+        clusters = [c.split('/')[2] for c in clusters]
 
-    results = pd.read_csv(catalog)
-    # mask out the missing values
-    mask = ~np.isnan(results['redshift'])
-    results = results[mask]
+    elif len(sys.argv) == 2:
+        clusters = [sys.argv[1]]
 
-    # now we start the process
-    separated = findseparationSpatial(results, center)
-    # add column for interlopers
+    for c in clusters:
+        print c
+        catalog = './../redshifts/'+c+'_redshifts.csv'
+        print catalog
+        # get the center
+        with open('./../centers/'+c+'_center.list', 'r') as f:
+            line = f.read()
+        line = line.split(' ')
+        center = float(line[0]), float(line[1])
 
-    galaxyNum = separated.shape[0]
+        results = pd.read_csv(catalog)
+        # mask out the missing values
+        mask = ~np.isnan(results['redshift'])
+        results = results[mask]
 
-    if galaxyNum >= 20:
-        while True:
-            try:
-                avgz = findClusterCenterRedshift(cleaned)
-                losv = findLOSV(cleaned, avgz)
-                losv['interloper'] = 'NO'
-            except NameError:
-                separated['interloper'] = 'NO'
-                members = separated.interloper.value_counts().NO
-                avgz = findClusterCenterRedshift(separated)
-                #avgz = 0.2256
-                losv = findLOSV(separated, avgz)
+        # now we start the process
+        separated = findseparationSpatial(results, center)
+        # add column for interlopers
 
-            rejectInterlopers(losv)
-            cleaned = losv
+        galaxyNum = separated.shape[0]
 
-            print avgz
-            if members == cleaned.interloper.value_counts().NO:
-                break
-            else:
-                members = cleaned.interloper.value_counts().NO
+        if galaxyNum >= 20:
+            while True:
+                try:
+                    avgz = findClusterCenterRedshift(cleaned)
+                    losv = findLOSV(cleaned, avgz)
+                    losv['interloper'] = 'NO'
+                except NameError:
+                    separated.loc[:, 'interloper'] = 'NO'
+                    members = separated.interloper.value_counts().NO
+                    #avgz = findClusterCenterRedshift(separated)
+                    avgz = 0.2256
+                    losv = findLOSV(separated, avgz)
 
-    else:
-        while True:
-            try:
-                avgz = findClusterCenterRedshift(cleaned)
-                losv = findLOSV(cleaned, avgz)
-                losv['interloper'] = 'NO'
-            except NameError:
-                separated['interloper'] = 'NO'
-                members = separated.interloper.value_counts().NO
-                avgz = findClusterCenterRedshift(separated)
-                losv = findLOSV(separated, avgz)
-
-            try:
-                rejectInterlopers_group(losv, avgz, sigmav=LOSVD)
+                mask = shifty_gapper(losv.separation.values,
+                        losv.redshift.values, avgz, ngap=20, vlimit=5000)
+                #rejectInterlopers(losv)
+                losv.loc[~mask, 'interloper'] = 'YES'
                 cleaned = losv
-                LOSVD = 1.135 *\
-                ast.gapperEstimator(cleaned.LOSV[cleaned.interloper == 'NO'])
-                print LOSVD
-            except NameError:
-                rejectInterlopers_group(losv, avgz)
-                cleaned = losv
-                LOSVD = 1.135 *\
-                ast.gapperEstimator(cleaned.LOSV[cleaned.interloper == 'NO'])
-                print LOSVD
 
-            print avgz
-            if members == cleaned.interloper.value_counts().NO:
-                break
-            else:
-                members = cleaned.interloper.value_counts().NO
+                print avgz
+                if members == cleaned.interloper.value_counts().NO:
+                    break
+                else:
+                    members = cleaned.interloper.value_counts().NO
 
+        else:
+            while True:
+                try:
+                    avgz = findClusterCenterRedshift(cleaned)
+                    losv = findLOSV(cleaned, avgz)
+                    losv['interloper'] = 'NO'
+                except NameError:
+                    separated['interloper'] = 'NO'
+                    members = separated.interloper.value_counts().NO
+                    avgz = findClusterCenterRedshift(separated)
+                    losv = findLOSV(separated, avgz)
 
+                try:
+                    rejectInterlopers_group(losv, avgz, sigmav=LOSVD)
+                    cleaned = losv
+                    LOSVD = 1.135 *\
+                    ast.gapperEstimator(cleaned.LOSV[cleaned.interloper == 'NO'])
+                    print LOSVD
+                except NameError:
+                    rejectInterlopers_group(losv, avgz)
+                    cleaned = losv
+                    LOSVD = 1.135 *\
+                    ast.gapperEstimator(cleaned.LOSV[cleaned.interloper == 'NO'])
+                    print LOSVD
 
-    cleaned.to_csv(sys.argv[1]+'_members.csv', index=False)
+                print avgz
+                if members == cleaned.interloper.value_counts().NO:
+                    break
+                else:
+                    members = cleaned.interloper.value_counts().NO
+
+        cleaned.to_csv(c+'_members.csv', index=False)
+        del cleaned
