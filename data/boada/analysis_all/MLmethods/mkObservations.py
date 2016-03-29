965dc4e1cb5e684b758da0277af453af93ef5e35
Steven <stevenboada@gmail.com>
2016-03-29 16:22:49 -0500

    bunch of changes
    creating a restore point for this thingy.

--- data/boada/analysis_all/MLmethods/mkObservations.py
+++ data/boada/analysis_all/MLmethods/mkObservations.py
@@ -4,9 +4,11 @@ from glob import glob
 import numpy as np
 from itertools import combinations
 from sklearn.cross_validation import train_test_split
-from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
+from sklearn.grid_search import RandomizedSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.feature_selection import RFECV
 from scipy.stats import randint as sp_randint
 import h5py as hdf
 
@@ -29,33 +31,35 @@ features = pd.DataFrame()
 features = result[['u','g','r', 'i', 'z']]
 
 # colors
-colors = combinations('zirgu', 2)
+colors = combinations('ugriz', 2)
 for i in colors:
     features['%s-%s' % (i[0], i[1])] = result.loc[:,i[0]] - result.loc[:,i[1]]
 
 # colors squared
-colors = combinations('zirgu', 2)
-# for i in colors:
-#     features['(%s-%s)2' % (i[0], i[1])] = (result.loc[:,i[0]] -
-#             result.loc[:,i[1]])**2
+colors = combinations('ugriz', 2)
+for i in colors:
+    features['(%s-%s)2' % (i[0], i[1])] = (result.loc[:,i[0]] -
+            result.loc[:,i[1]])**2
 
 # make datasets
 X = features.values
 y = result.Q.values
+scaler = preprocessing.StandardScaler().fit(X)
+X = scaler.transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X,y,
-        test_size=0.50)
-
+        test_size=0.30)
 
 # # use a full grid over all parameters -- for the random search
 param_grid = {"max_depth": [3, None],
-              "max_features": sp_randint(1, 15),
-              "min_samples_split": sp_randint(1, 15),
+              #"max_features": sp_randint(1, 25),
+              'max_features': ['auto'],
+              "min_samples_split": sp_randint(1, 25),
               "min_samples_leaf": sp_randint(1, 10),
               "bootstrap": [True, False],
               "criterion": ["gini", "entropy"],
               "n_estimators": sp_randint(5, 100)}
 
-scores = ['recall']
+scores = ['precision', 'recall']
 
 for score in scores:
     print("# Tuning hyper-parameters for %s" % score)
@@ -67,35 +71,38 @@ for score in scores:
 
     # perform a randomized grid search for the best possible 
     n_iter_search = 50
-    clf = RandomizedSearchCV(ExtraTreesClassifier(),
+    clf = RandomizedSearchCV(RandomForestClassifier(),
             param_distributions=param_grid,
             n_iter=n_iter_search, cv=5, scoring='accuracy',
             n_jobs=-1)
 
-    clf.fit(X, y)
-
+    clf.fit(X_train, y_train)
+    
     print("Best parameters set found on development set:")
     print()
     print(clf.best_params_)
     print()
-    print("Grid scores on development set:")
-    print()
-    for params, mean_score, scores in clf.grid_scores_:
-        print("%0.3f (+/-%0.03f) for %r"
-             % (mean_score, scores.std() * 2, params))
+    # print("Grid scores on development set:")
+    # print()
+    # for params, mean_score, scores in clf.grid_scores_:
+    #     print("%0.3f (+/-%0.03f) for %r"
+    #          % (mean_score, scores.std() * 2, params))
     #print()
 
-    # print("Detailed classification report:")
-    # print()
-    # print("The model is trained on the full development set.")
-    # print("The scores are computed on the full evaluation set.")
-    # print()
-    # y_true, y_pred = y_test, clf.predict(X_test)
-    # print(classification_report(y_true, y_pred))
-    # print()
-    # print(confusion_matrix(y_true, y_pred))
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
+    print(confusion_matrix(y_true, y_pred))
 
-for j in range(10):
+# now train on all of the data
+clf.fit(X,y)
+
+for j in range(5):
     print(j)
     magDict = {}
     with hdf.File('./truth/truth'+str(j).zfill(2)+'_Oii.hdf5', 'r') as f:
@@ -111,19 +118,22 @@ for j in range(10):
     print(mask.size)
     # create a data array for everything to fit into
     #features = -np.ones((mask.size, 25))
-    features = -np.ones((mask.size, 15))
+    features = -np.ones((mask.size, 25))
 
     # mags
     for i, m in enumerate('ugriz'):
         features[:,i] = magDict[m][mask]
     
     # colors
-    colors = combinations('zirgu', 2)
+    colors = combinations('ugriz', 2)
     for i, c in enumerate(colors):
         features[:,i+5] = magDict[c[0]][mask] - magDict[c[1]][mask]
         # colors squared
-        #features[:,i+15] = (magDict[c[0]][mask] - magDict[c[1]][mask])**2
+        features[:,i+15] = (magDict[c[0]][mask] - magDict[c[1]][mask])**2
 
+    
+    features = scaler.transform(features)
+    features = rfecv.transform(features)
     # now we make the predictions based on the new features we've created
     Qs = clf.predict(features)
 
@@ -135,6 +145,3 @@ for j in range(10):
     #     values = -np.ones(magDict['u'].size)
     #     values[mask] = Qs
     #     f['Q'] = values
-
-
-        
