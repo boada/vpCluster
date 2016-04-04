54327de5faf2560ac2761127de1a780e2a627255
Steven <stevenboada@gmail.com>
2016-04-04 17:41:52 -0500

    updated plotting function

--- data/boada/analysis_all/plotting/plot_massRichness.py
+++ data/boada/analysis_all/plotting/plot_massRichness.py
@@ -1,31 +1,46 @@
+from __future__ import division
 import pylab as pyl
+import h5py as hdf
+
+def mkError(x, x_err):
+    return 0.434 * x_err/x
+
+with hdf.File('./../results_cluster.hdf5', 'r') as f:
+    dset = f[f.keys()[0]]
+    data = dset.value
+    data.sort(order='ID')
 
 richnessData = pyl.genfromtxt('./boada_rich.txt', names=True, dtype=None)
-massData = pyl.genfromtxt('./cluster_props', names=True, dtype=None)
+richnessData.sort(order='name')
 
-richnessData.sort()
-massData.sort()
 
-yerr = [(massData['mass']-massData['mass_lower'])*10, abs(massData['mass'] -
-    massData['mass_upper'])*10]
+mass = pyl.log10(data['MASS'])
+yerr = mkError(data['MASS'], data['MASS_err'])
 
 ax = pyl.subplot()
 
-ax.errorbar(richnessData['lambda'], massData['mass']*10,
-        xerr=richnessData['lambda_err'], yerr=yerr, fmt='o', label='This Work')
+ax.errorbar(pyl.log10(richnessData['lambda']), mass,
+        xerr=mkError(richnessData['lambda'], richnessData['lambda_err']),
+        yerr=yerr, fmt='o', label='This Work')
 
-ax.set_xlabel('Richness')
-ax.set_ylabel(r'Cluster Mass $[10^{14}M_{\odot}]$')
+ax.set_xlabel('Log Richness')
+ax.set_ylabel(r'Log Cluster Mass $[M_{\odot}]$')
 
 # add the Rozo2010 points
-
 rozo = pyl.genfromtxt('./rozo2010_points', names=True, dtype=None)
-ax.errorbar(rozo['lambda'], rozo['mass']*10,
-        xerr=rozo['lambda_err'], fmt='o', label='Rozo+2010')
+ax.errorbar(pyl.log10(rozo['lambda']), pyl.log10(rozo['mass']*1e15),
+        xerr=mkError(rozo['lambda'], rozo['lambda_err']), fmt='o',
+        label='Rozo+2010')
+
+# add the masses from Sifon2015
+sifonMasses = pyl.array([14.6e14, 4.5e14,17.0e14, 7.0e14])
+sifonMass_err = pyl.array([3.1e14, 1.9e14, 2.8e14, 2.0e14])
+sifonRichness = pyl.array([190.996, 135.59, 202.636, 185.764])
+ax.errorbar(pyl.log10(sifonRichness), pyl.log10(sifonMasses),
+        yerr=mkError(sifonMasses, sifonMass_err), fmt='o', color='0.8',
+        label='Sifon+2015')
 
 pyl.legend(loc='lower right')
 
-ax.set_ylim(1e-2, 50)
-pyl.semilogy()
 pyl.show()
 
