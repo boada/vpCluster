19ee97fa6a27061e8bb252c19b375fc1e59222e1
Steven <stevenboada@gmail.com>
2016-04-22 17:11:53 -0500

    remove literature points

--- data/boada/analysis_all/plotting/plot_massRichness.py
+++ data/boada/analysis_all/plotting/plot_massRichness.py
@@ -27,18 +27,18 @@ ax.set_xlabel('Log Richness')
 ax.set_ylabel(r'Log Cluster Mass $[M_{\odot}]$')
 
 # add the Rozo2010 points
-rozo = pyl.genfromtxt('./rozo2010_points', names=True, dtype=None)
-ax.errorbar(pyl.log10(rozo['lambda']), pyl.log10(rozo['mass']*1e15),
-        xerr=mkError(rozo['lambda'], rozo['lambda_err']), fmt='o',
-        label='Rozo+2010')
+#rozo = pyl.genfromtxt('./rozo2010_points', names=True, dtype=None)
+#ax.errorbar(pyl.log10(rozo['lambda']), pyl.log10(rozo['mass']*1e15),
+#        xerr=mkError(rozo['lambda'], rozo['lambda_err']), fmt='o',
+#        label='Rozo+2010')
 
 # add the masses from Sifon2015
-sifonMasses = pyl.array([14.6e14, 4.5e14,17.0e14, 7.0e14])
-sifonMass_err = pyl.array([3.1e14, 1.9e14, 2.8e14, 2.0e14])
-sifonRichness = pyl.array([190.996, 135.59, 202.636, 185.764])
-ax.errorbar(pyl.log10(sifonRichness), pyl.log10(sifonMasses),
-        yerr=mkError(sifonMasses, sifonMass_err), fmt='o', color='0.8',
-        label='Sifon+2015')
+#sifonMasses = pyl.array([14.6e14, 4.5e14,17.0e14, 7.0e14])
+#sifonMass_err = pyl.array([3.1e14, 1.9e14, 2.8e14, 2.0e14])
+#sifonRichness = pyl.array([190.996, 135.59, 202.636, 185.764])
+#ax.errorbar(pyl.log10(sifonRichness), pyl.log10(sifonMasses),
+#        yerr=mkError(sifonMasses, sifonMass_err), fmt='o', color='0.8',
+#        label='Sifon+2015')
 
 pyl.legend(loc='lower right')
 
