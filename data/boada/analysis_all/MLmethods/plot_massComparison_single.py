d0ddc9b0b2c6267e1ea9b9fae2199ab8f5224dd4
Steven <stevenboada@gmail.com>
2016-05-24 20:53:18 -0500

    updated plot with 2d2 masses

--- data/boada/analysis_all/MLmethods/plot_massComparison_single.py
+++ data/boada/analysis_all/MLmethods/plot_massComparison_single.py
@@ -22,7 +22,7 @@ ax1s.yaxis.set_minor_locator(AutoMinorLocator())
 ################
 with hdf.File('./buzzard_targetedRealistic_shifty_masses.hdf5', 'r') as f:
     dset = f[f.keys()[0]]
-    target = dset['M200c', 'MASS', 'ML_pred_1d', 'ML_pred_2d', 'ML_pred_3d']
+    target = dset['M200c', 'MASS', 'ML_pred_1d', 'ML_pred_2d2', 'ML_pred_3d']
 # filter bad values
 mask = (target['ML_pred_1d'] != 0)
 target = target[mask]
@@ -41,7 +41,7 @@ zo = 0
 
 print('power law')
 y_ = astStats.runningStatistic(pyl.log10(target['M200c']),
-        pyl.log10(target['MASS']), pyl.percentile, binNumber=20, q=[16, 50, 84])
+        pyl.log10(target['MASS']), pyl.percentile, binNumber=15, q=[16, 50, 84])
 quants = pyl.array(y_[1])
 ax1.plot(y_[0],quants[:,1], '-', c=c, zorder=zo)
 
@@ -50,7 +50,7 @@ if not c == '#e24a33':
         alpha=0.3, edgecolor=c)
 err = calc_err(target['MASS'], target['M200c'])
 y_ = astStats.runningStatistic(pyl.log10(target['M200c']), err,
-        pyl.percentile, binNumber=20, q=[16, 50, 84])
+        pyl.percentile, binNumber=15, q=[16, 50, 84])
 quants = pyl.array(y_[1])
 ax1s.plot(y_[0],quants[:,1], '-', c=c, zorder=zo)
 
@@ -65,17 +65,17 @@ if not c == '#e24a33':
 c = '#467821'
 zo = 1
 
-print('3d')
-y_ = astStats.runningStatistic(pyl.log10(target['M200c']), target['ML_pred_3d'],
-        pyl.percentile, binNumber=20, q=[16, 50, 84])
+print('2d2')
+y_ = astStats.runningStatistic(pyl.log10(target['M200c']), target['ML_pred_2d2'],
+        pyl.percentile, binNumber=15, q=[16, 50, 84])
 quants = pyl.array(y_[1])
 ax1.plot(y_[0],quants[:,1], '--', c=c, zorder=zo)
 if not c == '#e24a33':
     ax1.fill_between(y_[0], quants[:,2], quants[:,0], facecolor=c,
         alpha=0.4, edgecolor=c)
-err = calc_err(10**target['ML_pred_3d'], target['M200c'])
+err = calc_err(10**target['ML_pred_2d2'], target['M200c'])
 y_ = astStats.runningStatistic(pyl.log10(target['M200c']), err,
-        pyl.percentile, binNumber=20, q=[16, 50, 84])
+        pyl.percentile, binNumber=15, q=[16, 50, 84])
 quants = pyl.array(y_[1])
 ax1s.plot(y_[0],quants[:,1], '--', c=c, zorder=zo)
 if not c == '#e24a33':
@@ -87,7 +87,7 @@ if not c == '#e24a33':
 ##################
 line1 = pyl.Line2D([], [], ls='-', color='#cf4457')
 line2 = pyl.Line2D([], [], ls='--', color='#467821')
-ax1.legend([line1, line2], ['Power Law', '$ML_{\sigma, z, Ngal}$'], loc=2)
+ax1.legend([line1, line2], ['Power Law', '$ML_{\sigma, Ngal}$'], loc=2)
 
 #### tweak ####
 ax1.set_xticks([12,13,14,15])
