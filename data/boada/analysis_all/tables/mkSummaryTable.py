50423634e07405c23f0b3a69e1b0a910701e2d77
Steven <stevenboada@gmail.com>
2016-05-24 20:53:55 -0500

    updated table maker with ML masses

--- data/boada/analysis_all/tables/mkSummaryTable.py
+++ data/boada/analysis_all/tables/mkSummaryTable.py
@@ -5,13 +5,27 @@ with hdf.File('./../results_cluster.hdf5', 'r') as f:
     dset = f[f.keys()[0]]
     results = dset.value
 
+with hdf.File('./../MLmethods/ML_predicted_masses.hdf5', 'r') as f:
+    dset = f[f.keys()[0]]
+    ML = dset.value
+
+
 for i in range(results.size):
     print('%s & %d & %d (%d) & %d & %.4f$\pm{%.3f}$ & %d$\pm{%d}$ & '\
-    r'%.2f$\pm{%.2f}$ \\' % (results['ID'][i], results['SOURCES'][i],
-    results['Q0'][i], results['Q1'][i], results['MEMBERS'][i],
-    results['Zc'][i], results['Zc_err'][i],
-    results['LOSVD'][i], results['LOSVD_err'][i], results['MASS'][i]/1e14,
-    results['MASS_err'][i]/1e14))
+    r'%.2f$\pm{%.2f}$ & %.2f$\pm{%.2f}$ \\' % (results['ID'][i],
+        results['SOURCES'][i],
+    results['Q0'][i],
+    results['Q1'][i],
+    results['MEMBERS'][i],
+    results['Zc'][i],
+    results['Zc_err'][i],
+    results['LOSVD'][i],
+    results['LOSVD_err'][i],
+    results['MASS'][i],
+    results['MASS_err'][i],
+    ML['ML_pred_3d'][i],
+    ML['ML_pred_3d_err'][i]
+    ))
 
 
 
