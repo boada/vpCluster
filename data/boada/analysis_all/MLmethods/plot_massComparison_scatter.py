f4ee7a0b359ba2b87170d8d9c7e9a89b8506aaaa
Steven <stevenboada@gmail.com>
2016-11-28 13:40:26 -0500

    cleaned up for publication
    make the figure more publication ready instead of testing ready

--- data/boada/analysis_all/MLmethods/plot_massComparison_scatter.py
+++ data/boada/analysis_all/MLmethods/plot_massComparison_scatter.py
@@ -9,18 +9,34 @@ with hdf.File('../results_cluster.hdf5', 'r') as f:
     dset = f[f.keys()[0]]
     results = dset.value
 
-for f,c in zip(files, 'rgb'):
+# make a figure
+f = pyl.figure(figsize=(6,6*(pyl.sqrt(5.)-1.0)/2.0))
+ax = f.add_subplot(111)
+
+i=0
+for f, c, l in zip(files, ['#7a68a6', '#e24a33'],
+        ['$ML_{\sigma, N_{gals}}$, Flat HMF',
+            '$ML_{\sigma, N_{gals}}$']):
+
+    if i==0:
+        i+=1
+        continue
 
     with hdf.File(f, 'r') as f1:
         dset = f1[f1.keys()[0]]
         ML = dset.value
 
-    pyl.errorbar(results['MASS'], ML['ML_pred'], xerr=results['MASS_err'],
-            yerr=ML['ML_pred_err'], fmt='o', color=c, markersize=10,
-            label=f.rstrip('.hdf5'))
+    ax.errorbar(results['MASS'], ML['ML_pred'], xerr=results['MASS_err'],
+        yerr=ML['ML_pred_err'], fmt='o', color=c, markersize=10,
+        label=l)#f.rstrip('.hdf5'))
 
-pyl.xlabel('M$_{pred, PL}$')
-pyl.ylabel('M$_{pred, ML}$')
-pyl.plot([12,16],[12,16], c='k', zorder=0)
+ax.set_xlabel('Log M$_{pred, PL}$')
+ax.set_ylabel('Log M$_{pred, ML}$')
+ax.plot([12.5,15.5],[12.5,15.5], c='k', zorder=0)
+
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles[::-1], labels[::-1], loc='upper left')
 
 
+
+pyl.show()
